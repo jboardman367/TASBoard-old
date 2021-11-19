4a9f583a8de6b6a9d3e3af13c197b6a0ac46f4b6
jboardman367@gmail.com <jboardman367@gmail.com>
2021-11-19 11:50:36 +1000

    Add project files.

---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TASBoard.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
